e28af1c5991cf78b8e35ec7f16b8ece041bd0d63
Maria Sideri <maria.sideri@consolut.com>
2023-08-11 12:18:42 +0000

    First Commit

---
+++ workflow-module/scripts/CreateSalesOrder.js
@@ -0,0 +1,24 @@
+$.context.SalesOrderHeader = {
+
+	"request" : {},
+ 
+	"response" : {}
+ 
+ }
+
+var body = {
+
+	"SoldToParty": $.context.salesOrderDetails.shipToParty,
+
+	"SalesOrderType": $.context.salesOrderDetails.salesOrderType,
+
+	"TotalNetAmount": $.context.salesOrderDetails.orderAmount.toString(),
+
+	"SalesOrganization": $.context.salesOrderDetails.salesOrganisation,
+
+	"DistributionChannel": $.context.salesOrderDetails.distributionChannel,
+
+	"OrganizationDivision": $.context.salesOrderDetails.division
+
+}
+$.context.SalesOrderHeader.request = body
